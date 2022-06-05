d8b4d1a560c33e16f21f2c762332e91d6689ded9
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2022-06-05 13:43:58 +0300

    cpp cmake

--- Core/Inc/dht_if.h
+++ Core/Inc/dht_if.h
@@ -1,9 +1,17 @@
 #ifndef __DHT_IF_H_
 #define __DHT_IF_H_
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
 #include "dht.h"
 #include <wiringPi.h>
 #include <unistd.h>
+
+#ifdef __cplusplus
+}
+#endif
 
 
 #endif
