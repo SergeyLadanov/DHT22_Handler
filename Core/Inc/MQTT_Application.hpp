5e757cd2c4d148ceaefb5b6e3d252a10022eaaab
Sergey Ladanov <ladanov@digitrans.ru>
2023-08-09 16:37:12 +0300

    add

--- Core/Inc/MQTT_Application.hpp
+++ Core/Inc/MQTT_Application.hpp
@@ -1,6 +1,23 @@
 #ifndef __MQTT_APPLICATION_HPP_
 #define __MQTT_APPLICATION_HPP_
 
+#include <cstdint>
+#include "MQTT_Client.hpp"
+
+class MQTT_Application
+{
+public:
+    
+private:
+    struct Topics_t
+    {
+        char TempTopic[32];
+        char HumTopic[32];
+    };
+
+    static MQTT_Client MQTT_Hanlder;
+    static Topics_t Topics;
+};
 
 
 #endif
--- Core/Src/MQTT_Application.cpp
+++ Core/Src/MQTT_Application.cpp
@@ -1,1 +1,5 @@
 #include "MQTT_Application.hpp"
+
+
+MQTT_Client MQTT_Application::MQTT_Hanlder("DHT22_1");
+MQTT_Application::Topics_t MQTT_Application::Topics;
